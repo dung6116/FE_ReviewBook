0135b95cd592091819816ff814e3f1e6eada07ca
dung6116 <duongthanhdung612@gmail.com>
2022-05-25 09:41:06 +0700

    Merge 2 phan Admin va User

---
+++ assets/js/file-upload copy.js
@@ -0,0 +1,15 @@
+(function($) {
+  'use strict';
+  $(function() {
+    $('.file-upload-browse').on('click', function() {
+      var file = $(this).parent().parent().parent().find('.file-upload-default');
+      file.trigger('click');
+    });
+    $('.file-upload-default').on('change', function() {
+      // $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+      $(this).attr('src', path.replace("image", "http://mywebsite.com/folder"));
+      document.getElementById("bookImgId").src="file";
+
+    });
+  });
+})(jQuery);

7580f50bde761071aca93c4b8e2c24b2c8fee970
dung6116 <duongthanhdung612@gmail.com>
2022-06-06 08:30:49 +0700

    Fix Xem thong tin tac gia, the loai, nxb

--- user-js/role-user.js
+++ user-js/role-user.js
@@ -12,3 +12,17 @@ function xem_chi_tiet_theloai(id){
     sessionStorage.setItem("id_tag",id);
     window.location ="../user_book-category.html"
 }
+
+function xem_chi_tiet_nxb(id){
+    sessionStorage.setItem("id_nxb",id);
+    window.location ="../user_book-publisher.html"
+}
+
+function xem_chi_tiet_friend(id){
+    if (id == sessionStorage.getItem("id")){
+        window.location ="../user_info.html"
+    } else {
+        sessionStorage.setItem("id_friend",id);
+        window.location ="../user_info-friend.html"
+    }  
+}

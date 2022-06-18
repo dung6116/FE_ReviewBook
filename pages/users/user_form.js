525c11cfc93ec8311bc93626f2dfd54370d2cfb3
Minh Nguyet <dtmnguyet.qn@gmail.com>
2022-06-19 00:38:58 +0700

    DAC-236/add-new-user

--- pages/users/user_form.js
+++ pages/users/user_form.js
@@ -1,3 +1,38 @@
 function get_id_user(id){
     sessionStorage.setItem("id_user",id);
 }
+
+const image_input = document.getElementById("img-btn");
+image_input.addEventListener("change", function() {
+const reader = new FileReader();
+reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    //console.log(uploaded_image);
+    document.getElementById("user-img").style.backgroundImage = `url(${uploaded_image})`;
+});
+reader.readAsDataURL(this.files[0]);
+});
+
+function validate() {
+    if( document.getElementById("userName").value == "" ) {
+        alert( "Please provide username!" );
+        document.getElementById("userName").focus() ;
+        return false;
+    }
+    if( document.getElementById("password").value == "" ) {
+        alert( "Please provide password!");
+        document.getElementById("password").focus() ;
+        return false;
+    }
+    if( document.getElementById("confirmPass").value == "" ) {
+        alert( "Please confirm password!");
+        document.getElementById("confirmPass").focus() ;
+        return false;
+    } 
+    if (document.getElementById("password").value != document.getElementById("confirmPass").value){
+        alert( "Confirm password isn't correct!");
+        document.getElementById("confirmPass").focus() ;
+        return false;
+    }
+    return( true );
+ }
